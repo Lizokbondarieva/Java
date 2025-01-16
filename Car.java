6139ee6f0a5b18b4a9d61b26f3bbd6520a5d6a71
Lizokbondarieva <lizokbondarieva@gmail.com>
2025-01-16 11:23:17 +0200

    New class Car

--- Car.java
+++ Car.java
@@ -1,2 +1,29 @@
-public class Car {
+class Car {
+    String make;//марка
+    String model;//модель
+    int year; //рік випуску
+    double mileage;// пробіг
+
+    void increaseMileage(){
+        double additionalMileage=10;
+        mileage += additionalMileage;
+    }
+    void displayInfo(){
+        System.out.println("make: "+ make);
+        System.out.println("model: "+ model);
+        System.out.println("year:" + year);
+        System.out.println("mileage" + mileage);
+    }
+}
+class CarDemo {
+    public static void main(String[] args) {
+        Car mycar = new Car();
+        mycar.make = "Audi";
+        mycar.model = "hjdk";
+        mycar.year = 2023;
+        mycar.mileage = 3000;
+        mycar.increaseMileage();
+        mycar.displayInfo();
+
+    }
 }
